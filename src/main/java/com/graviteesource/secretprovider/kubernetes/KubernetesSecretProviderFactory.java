4f207849fb04e41f4b2e1f9214898004945019a0
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2023-09-14 10:43:40 +0200

    feat: secret provider plugin for Kubernetes

---
+++ src/main/java/com/graviteesource/secretprovider/kubernetes/KubernetesSecretProviderFactory.java
@@ -0,0 +1,18 @@
+package com.graviteesource.secretprovider.kubernetes;
+
+import com.graviteesource.secretprovider.kubernetes.config.K8sConfig;
+import io.gravitee.node.api.secrets.SecretProvider;
+import io.gravitee.node.api.secrets.SecretProviderFactory;
+
+/**
+ * @author Benoit BORDIGONI (benoit.bordigoni at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class KubernetesSecretProviderFactory
+  implements SecretProviderFactory<K8sConfig> {
+
+  @Override
+  public SecretProvider create(K8sConfig kubernetesConfiguration) {
+    return new KubernetesSecretProvider(kubernetesConfiguration);
+  }
+}
