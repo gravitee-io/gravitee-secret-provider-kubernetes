213f6e085f47d07651d99c087960b44322fda4c0
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2023-09-14 10:43:40 +0200

    feat: secret provider plugin for Kubernetes

---
+++ src/main/java/com/graviteesource/secretprovider/kubernetes/client/K8sSecretWatchResult.java
@@ -0,0 +1,10 @@
+package com.graviteesource.secretprovider.kubernetes.client;
+
+import io.gravitee.node.api.secrets.model.SecretEvent;
+import io.kubernetes.client.openapi.models.V1Secret;
+
+/**
+ * @author Benoit BORDIGONI (benoit.bordigoni at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public record K8sSecretWatchResult(SecretEvent.Type type, V1Secret v1Secret) {}
