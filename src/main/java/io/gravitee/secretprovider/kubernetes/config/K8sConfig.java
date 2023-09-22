eec02266ee8d001b74aa145028d1328f52130c17
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2023-09-22 09:08:05 +0200

    fix: allow config to contain only strings

--- src/main/java/io/gravitee/secretprovider/kubernetes/config/K8sConfig.java
+++ src/main/java/io/gravitee/secretprovider/kubernetes/config/K8sConfig.java
@@ -1,5 +1,7 @@
 package io.gravitee.secretprovider.kubernetes.config;
 
+import static io.gravitee.node.api.secrets.util.ConfigHelper.getProperty;
+
 import io.gravitee.node.api.secrets.SecretManagerConfiguration;
 import java.util.Map;
 import java.util.Objects;
@@ -17,21 +19,21 @@ import lombok.experimental.FieldNameConstants;
 @FieldNameConstants(level = AccessLevel.PRIVATE)
 public class K8sConfig implements SecretManagerConfiguration {
 
-    private boolean enabled;
+    private final boolean enabled;
     private String kubeConfigFile;
     private int timeoutMs;
     private String namespace;
 
     // called by introspection
-    public K8sConfig(Map<String, Object> properties) {
-        Objects.requireNonNull(properties);
-        enabled = (boolean) properties.getOrDefault(Fields.enabled, false);
+    public K8sConfig(Map<String, Object> conf) {
+        Objects.requireNonNull(conf);
+        enabled = getProperty(conf, Fields.enabled, Boolean.class, false);
         if (!isEnabled()) {
             return;
         }
-        kubeConfigFile = (String) properties.getOrDefault(Fields.kubeConfigFile, "");
-        timeoutMs = (int) properties.getOrDefault(Fields.timeoutMs, 3000);
-        namespace = (String) properties.getOrDefault(Fields.namespace, "default");
+        kubeConfigFile = getProperty(conf, Fields.kubeConfigFile, String.class, "");
+        timeoutMs = getProperty(conf, Fields.timeoutMs, Integer.class, 3000);
+        namespace = getProperty(conf, Fields.namespace, String.class, "default");
     }
 
     public boolean isClusterBased() {
